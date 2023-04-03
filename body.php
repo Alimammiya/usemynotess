35cb0612583b76de3e79b78936af4b2bd39c579a
Alimam Miya <52057677+Alimammiya@users.noreply.github.com>
2023-04-04 00:59:39 +0530

    Add files via upload

---
+++ body.php
@@ -0,0 +1,27 @@
+<style type="text/css">
+#services{padding:60px 0 40px 0;box-shadow:inset 0 0 12px 0 rgba(0,0,0,.1)}#services .box{padding:30px;position:relative;overflow:hidden;border-radius:10px;margin:0 10px 40px 10px;background:#fff;box-shadow:0 10px 29px 0 rgba(68,88,144,.1);transition:all 0.3s ease-in-out}#services .box:hover{-webkit-transform:translateY(-5px);transform:translateY(-5px)}#services .icon{position:absolute;left:-10px;top:calc(50% - 32px)}#services .icon i{font-size:64px;line-height:1;transition:0.5s}#services .title{margin-left:40px;font-weight:700;margin-bottom:15px;font-size:18px}#services .title a{color:#111}#services .box:hover .title a{color:#007bff}#services .description{font-size:14px;margin-left:40px;line-height:24px;margin-bottom:0}#why-us{padding:60px 0}#why-us .card{background:#00458f;border-color:#00458f;border-radius:10px;margin:0 15px;padding:15px 0;text-align:center;color:#fff;transition:0.3s ease-in-out;height:100%}#why-us .card:hover{background:#2dc997;border-color:#003b7a}#why-us .card i{font-size:48px;padding-top:15px;color:#bfddfe}#why-us .card h4{font-size:22px;font-weight:600}#why-us .card p{font-size:15px;color:#d8eafe}#why-us .card .readmore{color:#fff;font-weight:600;display:inline-block;transition:0.3s ease-in-out;border-bottom:#00458f solid 2px}#why-us .card .readmore:hover{border-bottom:#fff solid 2px}#why-us .counters{padding-top:40px}#why-us .counters span{font-family:"Montserrat",sans-serif;font-weight:700;font-size:48px;display:block;color:#fff}#why-us .counters p{padding:0;margin:0 0 20px 0;font-family:"Montserrat",sans-serif;font-size:14px;color:#cce5ff}
+</style>
+<section id="hero">
+    <div class="hero-container">
+      <h1>Use My Notes</h1>
+      <h2>Your Best Education source</h2>      
+    </div>
+</section>
+<section id="why-us" class="wow fadeIn">
+      <div class="container">
+        <header class="section-header">
+          <h3 style="text-align:center;"><strong>Know About Use My Notes</strong></h3>
+      
+          <p style="text-align: justify;">Use My Notes is a website that offers a variety of tools and resources to help students, professionals, and anyone who takes notes to organize and optimize their note-taking process. The website offers several features that can help users take better notes and study more effectively.</p>
+
+<p style="text-align: justify;">One of the key features of Use My Notes is its note-taking app, which allows users to take notes on any device, whether it's a laptop, tablet, or smartphone. The app offers a range of formatting options, including bullet points, numbered lists, and headings, which can help users organize their notes more effectively. Additionally, the app offers the ability to tag notes, which can make it easier to find specific information later.</p>
+
+      <p style="text-align: justify;"><a href="https://usemynotes.com/">Use My Notes</a> also offers a flashcard maker tool, which allows users to create flashcards based on their notes. This can be a great way to review information and test your knowledge, whether you're preparing for a test or just trying to reinforce your understanding of a particular subject.</p>
+
+<p style="text-align: justify;">In addition to its note-taking and flashcard tools, UseMyNotes.com also offers a library of study guides and resources on a variety of subjects. These resources can be a great way to supplement your notes and help you better understand the material.</p>
+
+<p style="text-align: justify;">Overall, Use My Notes is a comprehensive resource for anyone looking to improve their note-taking skills and study more effectively. Whether you're a student or a professional, this website can help you optimize your note-taking process and get the most out of your study sessions.</p>
+
+        </header>
+        
+    </section> 
---
+++ network-security.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+  <title>Cryptography and Network Security course for free</title>
+  <meta charset="UTF-8">  
+  <meta content="width=device-width, initial-scale=1" name="viewport">
+  <meta name="author" content="Alimam Miya">
+  <link rel="canonical" href="https://usemynotess.pages.dev/network-security" />
+  <meta name="google-site-verification" content="G4OenlcwQ3DRWxk3Qqb_vLJaQ2sFpasRJym0gwqimko" />
+  <meta name="description" content="In this post, we will provide the Cryptography and Network Security Course for Free from beginner to advance, and you can learn - What you'll learn, the list of Course content, Requirements, Description, and Why learn Cryptography?" /> 
+    <body>
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="stylete.css" rel="stylesheet">
+    <body>
+  <header id="header">
+    <div class="container">
+      <div id="logo" class="pull-left">
+         <a href="https://usemynotess.pages.dev/"><img src="https://usemynotes.com/wp-content/uploads/2020/10/use-my-notes-logo.png" alt="Use My Notes" title="Use My Notes"></a>       
+      </div>
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">      
+          <li><a href="https://usemynotess.pages.dev/">Home</a></li>  
+          
+         <li><a href="https://usemynotess.pages.dev/network-security">Network Security</a></li>   
+         <!--  <li><a href="notes/notes">Notes</a></li>
+          <li><a href="old-paper/old-paper">Old Papers</a></li> 
+          <li><a href="tutorials/index">Tutorials</a></li>
+          <li><a href="contact-us">Contact Us</a></li>
+          <li><a href="about-us">About Us</a></li>  -->
+          
+        </ul>
+      </nav>
+    </div>
+  </header>  
+  <section id="hero">
+    <div class="hero-container">
+      <h1>Cryptography and Network Security course for free</h1>   
+    </div>
+</section><br/>
+<section id="why-us" class="wow fadeIn">
+      <div class="container">  
+        <!-- common header end-->
+<p dir="auto">In this post, we will provide the <a href="https://usemynotes.com/cryptography/">Cryptography and Network Security Course for Free</a> from beginner to advance, and you can learn - What you'll learn, the list of Course content, Requirements, Description, and Why learn Cryptography?</p>
+
+<h2 dir="auto" tabindex="-1">What you'll learn</h2>
+<ul dir="auto">
+  <li>Start a career in network security. Become a network security specialist.</li>
+  <li>How to become job-ready by understanding how really Cryptography and Network Security work behind the project.</li>
+  <li>Cryptography fundamentals: IDS, firewall, encryption techniques, description techniques, PGP encryption, and more.</li>
+  <li>How to think and work like a good Network security expert: researching, problem-solving, and, workflows.</li>
+  <li>Learn more than advanced levels concept: DSS, MAC, MD5, RSA algorithm, DES algorithm, and more.</li>
+  <li>Learn How does PGP encryption works?</li>
+  <li>The role of hashing in PGP Encryption and what are the uses of PGP Encryption.</li>
+</ul>
+<h2 dir="auto" tabindex="-1">Course content</h2>
+<ul dir="auto">
+  <li><a href="https://usemynotes.com/what-is-cryptography/">What is Cryptography?</a></li>
+  <li><a href="https://usemynotes.com/what-is-vpn-and-how-does-it-work/">What is VPN and How does it work?</a></li>
+  <li><a href="https://usemynotes.com/what-is-virus-worms-and-trojans/">What is Computer Virus, Worms and Trojans?</a></li>
+  <li><a href="https://usemynotes.com/what-is-intrusion-detection-system/">What is Intrusion Detection System [IDS]?</a></li>
+  <li><a href="https://usemynotes.com/what-is-firewall-and-its-types/">What is Firewall and its Types?</a></li>
+  <li><a href="https://usemynotes.com/what-is-network-address-translation/">What is Network Address Translation (NAT)?</a></li>
+  <li><a href="https://usemynotes.com/computer-network-attacks/">What is Computer Network Attacks?</a></li>
+  <li><a href="https://usemynotes.com/what-is-secure-shell/">What is Secure Shell or (SSH)?</a></li>
+  <li><a href="https://usemynotes.com/what-is-encryption-and-decryption-techniques/">What is Encryption and Decryption Techniques?</a></li>
+  <li><a href="https://usemynotes.com/what-is-pgp-encryption-and-hashing/">What is PGP Encryption and Hashing?</a></li>
+  <li><a href="https://usemynotes.com/what-is-ssl-secure-socket-layer/">What is SSL (Secure Socket Layer)?</a></li>
+  <li><a href="https://usemynotes.com/what-is-digital-signature-standard/">What is Digital Signature Standard [DSS]?</a></li>
+  <li><a href="https://usemynotes.com/message-authentication-code-mac/">Message Authentication Code (MAC)</a></li>
+  <li><a href="https://usemynotes.com/what-is-md5-message-digest-algorithm-5/">What is MD5 (Message Digest algorithm 5)?</a></li>
+  <li><a href="https://usemynotes.com/what-is-rsa-algorithm/">What is RSA Algorithm?</a></li>
+  <li><a href="https://usemynotes.com/what-is-des-algorithm/">What is DES Algorithm?</a></li>
+</ul>
+<h2 dir="auto" tabindex="-1">Requirements</h2>
+<ul dir="auto">
+  <li>You must have a computer/Laptop or smartphone for work.</li>
+  <li>You must have any browser (Chrome, Firefox, etc) to learn our tutorials.</li>
+  <li>You must have the basic skill of mathematical problem-solving.</li>
+  <li>You must know the basics of the operating system (OS) to learn Network security.</li>
+</ul>
+<h2 dir="auto" tabindex="-1">Description</h2>
+<p dir="auto">Have you never known networking on a computer and smart devices before, and thought or say that Cryptography is a good networking course to start with. so you think right</p>
+<p dir="auto">Maybe you have some good experience in operating systems and networks, but want to learn cryptography and network security course. This is a great networking course for your CV (Curriculum vitae).</p>
+<p dir="auto">Perhaps you are stuck in a low-paying networking job and want to move to a better, more senior position to learn cryptography and network security, this might help you.</p>
+
+<h2 dir="auto" tabindex="-1">Why learn Cryptography?</h2>
+<p dir="auto">Cryptography is often considered the mother of Operating systems (OS) ​​because many other networks ​​have been based on it.</p>
+<p dir="auto">Although Cryptography is complex and may be simple, it is one of the most powerful security methods. Given that it was created 32 years ago, it is still heavily used and is generally one of the great, most popular, and most widespread network security methods ​​in the world.</p>
+<p dir="auto">Learning Cryptography and network security can really make you stronger at security.</p>
+<p dir="auto">By learning how things really work "under the hood" and understanding the DES algorithm, RSA algorithm, MD5, and so on, you can create the more efficient algorithm, and get a huge advantage over other algorithms in the process.</p>
+<p dir="auto">If you want to be a better network security expert, Cryptography is a great way to start learning!</p>
+<p dir="auto">It does not omit the course details. You'll learn how to write a high-quality algorithm and be an excellent problem solver. This course not only exists as an algorithm in cryptography and network security, but it also includes all the details on the "why" you are doing. At the end of this course, you will be able to fully understand the concepts of cryptography and network security.</p>
+<p dir="auto">The Maine website is <a href="https://usemynotes.com/">Use My Notes</a></p></div></section>
+
+      </body>
+
+<?php
+include('footer.php'); 
+?>
